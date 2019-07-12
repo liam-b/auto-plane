e7131b7281e4c41a2c65958d2ec48a1e9f3b2bc8
liam-b <liam@anywhich.com>
2019-07-12 10:12:17 +1000

    moved to raspi suite

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const raspi = require('raspi')
 const pwm = require('./pwm.js')
 const Esc = require('./esc.js')
+const Servo = require('./servo.js')
 
-var pwmHat = new pwm.Hat(60)
-var esc = new Esc(pwmHat, 0)
-esc.setSpeed(80)
+raspi.init(() => {
+  const pwmHat = new pwm.Hat(60)
+  const esc = new Esc(pwmHat, 0)
+  const servo = new Servo(pwmHat, 1)
+
+  esc.setSpeed(0)
+  servo.rotateTo(100)
+})
