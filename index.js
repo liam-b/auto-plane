623ef0b75079bee9392bb299f21af6aca7f94f74
liam-b <liam@anywhich.com>
2019-07-12 11:03:28 +1000

    added serial tests

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const raspi = require('raspi')
+const Serial = require('raspi-serial').Serial
 const pwm = require('./pwm.js')
 const Esc = require('./esc.js')
 const Servo = require('./servo.js')
 
 raspi.init(() => {
+  var serial = new Serial()
+  serial.open(() => {
+    serial.on('data', (data) => {
+      process.stdout.write(data)
+    })
+
+    serial.write('Hello from raspi-serial')
+  })
+  
   const pwmHat = new pwm.Hat(60)
   const esc = new Esc(pwmHat, 0)
   const servo = new Servo(pwmHat, 1)
